7ff8016e4d6e64eeb9bf13b30cee39c310481f47
shivamdeswal5 <deswalworks@gmail.com>
2025-04-28 14:42:25 +0530

    basics done

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
-console.log("Hello");
+// console.log("Hello");
+// let age: number = 20;
+// interface User {
+//     name: string;
+//     id: number;
+// }
+// const user: User = {
+//     name: "Shivam",
+//     id: 119
+// }
+// type myBool = true | "False";
+// let ans: myBool = "False"
+// function getLength(obj: string | string[]) {
+//     console.log(obj.length)
+// }
+// getLength(["Shivam", "Deswal"]);
+// //make function return different values bases on type
+// function wrapInArray(obj: string | string[]) {
+//     if (typeof obj === "string") {
+//         return [obj];
+//     }
+//     return obj;
+// }
+// wrapInArray("fdb")
+// //classes and object conform shapes in similar way
+// interface Point {
+//     x: number;
+//     y: number;
+// }
+// function logPoint(p: Point) {
+//     console.log(`${p.x}, ${p.y}`);
+// }
+// const point = { x: 12, y: 26 };
+// logPoint(point);
+// class VirtualPoint {
+//     x: number;
+//     y: number;
+//     constructor(x: number, y: number) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
+// const newVPoint = new VirtualPoint(13, 56);
+// logPoint(newVPoint);
+var Name = "Shivam";
+var age = 25;
+var isStudent = true;
+var scores = [90, 85, 78];
+var person = ["Shivam", 21];
+var Day;
+(function (Day) {
+    Day["Sunday"] = "blue";
+    Day["Monday"] = "black";
+    Day["Tuesday"] = "orange";
+})(Day || (Day = {}));
+var today = Day.Monday;
+console.log(today);
+function greeMe(name) {
+    return "Good Morning ".concat(name);
+}
+var person1 = {
+    name: "Shivam Deswal",
+    age: 21
+};
+// classes and objects
+var Car = /** @class */ (function () {
+    function Car(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+    Car.prototype.displayInfo = function () {
+        return "Car is: ".concat(this.make, " and model is: ").concat(this.model);
+    };
+    return Car;
+}());
+var car1 = new Car("Mahindra", "Scorpio");
+console.log(car1);
+//generics
+function identity(arg) {
+    return arg;
+}
+var sum = identity(123);
+var str = identity("Hello");
+function printId(id) {
+    console.log(id);
+}
+printId(101);
+printId("xya");
--- index.ts
+++ index.ts
@@ -1,32 +1,151 @@
-console.log("Hello");
+// console.log("Hello");
 
-let age: number = 20;
+// let age: number = 20;
 
-interface User {
-    name: string;
-    id:number;
+// interface User {
+//     name: string;
+//     id: number;
+// }
+
+// const user: User = {
+//     name: "Shivam",
+//     id: 119
+// }
+
+// type myBool = true | "False";
+
+// let ans: myBool = "False"
+
+// function getLength(obj: string | string[]) {
+//     console.log(obj.length)
+// }
+
+// getLength(["Shivam", "Deswal"]);
+
+// //make function return different values bases on type
+// function wrapInArray(obj: string | string[]) {
+//     if (typeof obj === "string") {
+//         return [obj];
+//     }
+//     return obj;
+// }
+
+// wrapInArray("fdb")
+
+// //classes and object conform shapes in similar way
+// interface Point {
+//     x: number;
+//     y: number;
+// }
+// function logPoint(p: Point) {
+//     console.log(`${p.x}, ${p.y}`);
+// }
+// const point = { x: 12, y: 26 };
+// logPoint(point);
+
+// class VirtualPoint {
+//     x: number;
+//     y: number;
+
+//     constructor(x: number, y: number) {
+//         this.x = x;
+//         this.y = y;
+//     }
+// }
+
+// const newVPoint = new VirtualPoint(13, 56);
+// logPoint(newVPoint);
+
+let Name: string = "Shivam";
+let age:number = 25;
+let isStudent : boolean = true;
+
+let scores : number[] = [90,85,78];
+let person: [string,number] = ["Shivam",21];
+
+enum Day{
+    Sunday = "blue",
+    Monday = "black",
+    Tuesday= "orange"
 }
 
-const user : User = {
-    name: "Shivam",
-    id: 119
+let today = Day.Monday;
+console.log(today);
+
+function greeMe(name: string): string{
+    return `Good Morning ${name}`
+    
 }
 
-type myBool =  true | "False";
+// function combine(a:string | number, b: string | number) : string | number{
+//     return a + b;
+// }
 
-let ans : myBool = "False"
+// console.log(combine(5,5));
+// console.log(combine("Shivam",5));
 
-function getLength(obj:string | string[]){
-    console.log(obj.length)
+
+//Interface
+interface User{
+    name: string,
+    age: number,
 }
 
-getLength(["Shivam","Deswal"]);
+let person1 : User = {
+    name:"Shivam Deswal",
+    age: 21
+}
 
-function wrapInArray(obj: string | string[]) {
-    if (typeof obj === "string") {
-      return [obj];         
+// classes and objects
+class Car{
+    make: string;
+    model: string;
+
+    constructor (make: string , model: string){
+        this.make = make;
+        this.model = model;
     }
-    return obj;
+
+    displayInfo() : string{
+        return `Car is: ${this.make} and model is: ${this.model}`
+    }
 }
 
-wrapInArray("fdb")
+let car1 = new Car("Mahindra","Scorpio");
+console.log(car1)
+
+//generics
+function identity <T>(arg: T) : T{
+    return arg;
+}
+let sum = identity(123);
+let str = identity("Hello");
+
+function printId(id: string | number){
+    console.log(id);
+}
+printId(101);
+printId("xya");
+
+// intersection
+interface Person{
+    name: string;
+}
+
+interface Contact{
+    phone: string;
+}
+
+type PersonWithContact = Person & Contact;
+
+const contact : PersonWithContact ={
+    name: "Shivam Deswal",
+    phone:"8566665185"
+}
+
+// We can create our own type
+type Point = {x:number, y:number};
+
+// type assertions
+let someValue : any = "Hello, Typescript"
+let strLength : number = (someValue as string).length;
